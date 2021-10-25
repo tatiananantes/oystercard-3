7a18fa6a45ccb6e9cd751567f675fda212cac515
Sigita <sigita.zeb92@gmail.com>
2021-10-25 17:58:20 +0100

    Enforce maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,19 @@
 class Oystercard
   attr_reader :balance
+  CARD_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    @balance += amount
+    fail "The limit is #{CARD_LIMIT}, can not add more money on your oystercard!" if exceeds_limit?(amount)
+    @balance += amount 
+  end
+
+  private
+
+  def exceeds_limit?(amount)
+    @balance + amount > CARD_LIMIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,16 +4,19 @@ describe Oystercard do
   let(:card) { Oystercard.new }
 
   it 'has a balance of zero' do
-  expect(card.balance).to eq(0)
+    expect(card.balance).to eq 0
   end
 
   describe '#top_up' do
-
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'will top up the balance by 5' do
+      expect { card.top_up 5 }.to change { card.balance }.by 5
+    end
 
-      expect { card.top_up(5) }.to change { card.balance }.by 5
+    it 'throws an exception if new balance exceeds the limit' do
+      card.top_up(Oystercard::CARD_LIMIT)
+      expect { card.top_up 1 }.to raise_error "The limit is #{Oystercard::CARD_LIMIT}, can not add more money on your oystercard!"
     end
   end
 end
